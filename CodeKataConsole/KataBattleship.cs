5d52b106d0191aa6df9cee2bfd882f45782d20da
boogster23 <roberto.collantes@hotmail.com>
2025-01-31 15:53:43 -0600

    Used Unit Tests, Battleship board validator kata

---
+++ CodeKataConsole/KataBattleship.cs
@@ -0,0 +1,211 @@
+﻿using NUnit.Framework;
+
+namespace CodeKataConsole
+{
+    public class KataBattleship
+    {
+        public static bool ValidateBattlefield(int[,] field)
+        {
+            var ships = new ShipContainer();
+            var shipContainer = new List<int>();
+
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (field[j, i] != 0)
+                    {
+                        shipContainer.Add(i + (j * 10));
+                        continue;
+                    }
+                }
+            }
+
+            var shipSizeCounter = 1;
+            var shipContainerCounted = new List<int>();
+
+            for (int i = 0; i < shipContainer.Count; i++)
+            {
+                // check for adjacents
+                if (shipContainer.Contains(shipContainer[i] + 9) ||
+                    shipContainer.Contains(shipContainer[i] + 11))
+                    return false;
+
+                if (shipContainerCounted.Contains(shipContainer[i])) continue;
+
+                shipContainerCounted.Add(shipContainer[i]);
+
+                if ((i + 1 < shipContainer.Count) && shipContainer[i + 1] - shipContainer[i] == 1) // next to each other
+                {
+                    shipSizeCounter++;
+                    continue;
+                }
+
+                if (shipSizeCounter == 1) // vertical check
+                {
+                    var x = shipContainer[i];
+                    while (x < 100)
+                    {
+                        x += 10;
+
+                        if (shipContainer.Contains(x))
+                        {
+                            shipContainerCounted.Add(x);
+                            shipSizeCounter++;
+                            continue;
+                        }
+
+                        break;
+                    }
+                }
+
+                if (shipSizeCounter > 0)
+                {
+                    if (shipSizeCounter == 1)
+                        ships.Submarine++;
+                    else if (shipSizeCounter == 2)
+                        ships.Destroyer++;
+                    else if (shipSizeCounter == 3)
+                        ships.Cruiser++;
+                    else if (shipSizeCounter == 4)
+                        ships.Battleship++;
+                    else
+                        return false;
+
+                    shipSizeCounter = 1;
+                }
+            }
+
+            // Validate Ships
+            return ships.Submarine == 4 && ships.Destroyer == 3 && ships.Cruiser == 2 && ships.Battleship == 1;
+        }
+    }
+
+    public class ShipContainer
+    {
+        public int Submarine { get; set; } = 0;
+        public int Destroyer { get; set; } = 0;
+        public int Cruiser { get; set; } = 0;
+        public int Battleship { get; set; } = 0;
+    }
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void HorizontalTestCaseShouldReturnTrue()
+        {
+            int[,] field = new int[10, 10]
+                {
+                    {1, 0, 1, 0, 1, 0, 0, 1, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 1, 1, 0, 1, 1, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 0, 1, 1, 1, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                };
+
+            Assert.That(KataBattleship.ValidateBattlefield(field), Is.True);
+        }
+
+        [Test]
+        public void HorizontalTestCaseShouldReturnFalse()
+        {
+            int[,] field = new int[10, 10]
+                {
+                    {1, 0, 1, 0, 1, 0, 0, 1, 0, 1}, // extra submarine here
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 1, 1, 0, 1, 1, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 0, 1, 1, 1, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                };
+
+            Assert.That(KataBattleship.ValidateBattlefield(field), Is.False);
+        }
+
+        [Test]
+        public void VerticalTestCaseShouldReturnTrue()
+        {
+            int[,] field = new int[10, 10]
+                {
+                    {1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+                    {1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                };
+
+            Assert.That(KataBattleship.ValidateBattlefield(field), Is.False);
+        }
+
+        [Test]
+        public void VerticalTestCaseShouldReturnFalse()
+        {
+            int[,] field = new int[10, 10]
+                {
+                    {1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 0, 1, 1, 0, 0, 0, 0, 1},
+                    {1, 0, 0, 1, 1, 0, 0, 0, 0, 1},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                };
+
+            Assert.That(KataBattleship.ValidateBattlefield(field), Is.False);
+        }
+
+        [Test]
+        public void TestCase()
+        {
+            int[,] field = new int[10, 10]
+                      {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                      {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                      {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                      {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                      {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            Assert.That(KataBattleship.ValidateBattlefield(field), Is.True);
+        }
+
+        [Test]
+        public void ShipsInContactFirstCase()
+        {
+            int[,] field = new int[10, 10]
+            {
+                {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 0, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            };
+            Assert.That(KataBattleship.ValidateBattlefield(field), Is.False);
+        }
+    }
+}
--- CodeKataConsole/Program.cs
+++ CodeKataConsole/Program.cs
@@ -4,76 +4,76 @@ namespace CodeKataConsole
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine(SpinWords("Hey fellow warriors"));
-            var strValue = "Cat";
-            Console.WriteLine($"{strValue} is IsoGram: {IsIsogram(strValue)}");
+        //static void Main(string[] args)
+        //{
+        //    //Console.WriteLine(SpinWords("Hey fellow warriors"));
+        //    var strValue = "Cat";
+        //    Console.WriteLine($"{strValue} is IsoGram: {IsIsogram(strValue)}");
 
-            Console.WriteLine("Testing PrinterError");
-            var s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
-            Console.WriteLine($"PrinterError(s) : {PrinterError(s)}");
+        //    Console.WriteLine("Testing PrinterError");
+        //    var s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        //    Console.WriteLine($"PrinterError(s) : {PrinterError(s)}");
 
-            Console.WriteLine("Testing Narcissistic Number");
-            var narValue = 371;
-            Console.WriteLine($"Narcissistic value {narValue} : {Narcissistic(narValue)}");
+        //    Console.WriteLine("Testing Narcissistic Number");
+        //    var narValue = 371;
+        //    Console.WriteLine($"Narcissistic value {narValue} : {Narcissistic(narValue)}");
 
-            Console.WriteLine($"Disemvowel: {Disemvowel("No offense but,\nYour writing is among the worst I've ever read")}");
+        //    Console.WriteLine($"Disemvowel: {Disemvowel("No offense but,\nYour writing is among the worst I've ever read")}");
 
-            Console.WriteLine($"To JadenCase: {ToJadenCase("How can mirrors be real if our eyes aren't real")}");
+        //    Console.WriteLine($"To JadenCase: {ToJadenCase("How can mirrors be real if our eyes aren't real")}");
 
-            Console.WriteLine($"To Rgb String (300,101,50) : {Rgb(300, 10, -1)}");
+        //    Console.WriteLine($"To Rgb String (300,101,50) : {Rgb(300, 10, -1)}");
 
-            int[] a = { 121, 144, 19, 161, 19, 144, 19, 11 };
-            int[] b = { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+        //    int[] a = { 121, 144, 19, 161, 19, 144, 19, 11 };
+        //    int[] b = { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
 
-            Console.WriteLine($"Array Comp Test: {Comp(a, b)}");
+        //    Console.WriteLine($"Array Comp Test: {Comp(a, b)}");
 
 
-            string[] a1 = ["tarp", "mice", "bull"];
-            string[] a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
-            Console.WriteLine($"Lexicographical InArray Test: {string.Join(", ", InArray(a1, a2))}");
+        //    string[] a1 = ["tarp", "mice", "bull"];
+        //    string[] a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
+        //    Console.WriteLine($"Lexicographical InArray Test: {string.Join(", ", InArray(a1, a2))}");
 
-            int[] oddOutlierArray = [-5, 6, 6];
-            int[] evenOutlierArray = [160, 3, 1719, 19, 11, 13, -21];
+        //    int[] oddOutlierArray = [-5, 6, 6];
+        //    int[] evenOutlierArray = [160, 3, 1719, 19, 11, 13, -21];
 
-            Console.WriteLine($"Parity Outlier: {FindParityOutlier(oddOutlierArray)}");
+        //    Console.WriteLine($"Parity Outlier: {FindParityOutlier(oddOutlierArray)}");
 
-            int intForRomanConversion = 588;
-            Console.WriteLine($"Roman Converter ({intForRomanConversion}): {RomanConvert.Solution(intForRomanConversion)}");
+        //    int intForRomanConversion = 588;
+        //    Console.WriteLine($"Roman Converter ({intForRomanConversion}): {RomanConvert.Solution(intForRomanConversion)}");
 
-            int[,] board = new int[,] { { 1, 1, 1 }, { 0, 2, 2 }, { 0, 0, 0 } };
-            int[,] incBoard = new int[,] { { 0, 0, 2 }, { 0, 0, 0 }, { 1, 0, 1 } };
-            Console.WriteLine($"TicTacToe Result: {TicTacToe.IsSolved(incBoard)}");
+        //    int[,] board = new int[,] { { 1, 1, 1 }, { 0, 2, 2 }, { 0, 0, 0 } };
+        //    int[,] incBoard = new int[,] { { 0, 0, 2 }, { 0, 0, 0 }, { 1, 0, 1 } };
+        //    Console.WriteLine($"TicTacToe Result: {TicTacToe.IsSolved(incBoard)}");
 
-            Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
-                3, [1337, 256])}");
-            Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
-                1336, [1337, 256])}");
-            Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
-                1337, [1337, 256])}");
-            Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
-                100, [1337, 256])}");
-            Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
-                110101, [1337, 256])}");
-            Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
-                99, [1337, 256])}");
+        //    Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
+        //        3, [1337, 256])}");
+        //    Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
+        //        1336, [1337, 256])}");
+        //    Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
+        //        1337, [1337, 256])}");
+        //    Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
+        //        100, [1337, 256])}");
+        //    Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
+        //        110101, [1337, 256])}");
+        //    Console.WriteLine($"KataIsInterestingNumber Result: {KataIsInterestingNumber.IsInteresting(
+        //        99, [1337, 256])}");
 
-            Console.WriteLine($"Kata ROT13: {KataROT13.Rot13("EBG13 rknzcyr.")}");
-            Console.WriteLine($"Kata Top Words: {string.Join(", ", KataTopWords.Top3("  '''  "))}");
+        //    Console.WriteLine($"Kata ROT13: {KataROT13.Rot13("EBG13 rknzcyr.")}");
+        //    Console.WriteLine($"Kata Top Words: {string.Join(", ", KataTopWords.Top3("  '''  "))}");
 
-            Console.WriteLine($"Kata Human Readable Num: {KataHumanReadableTime.formatDuration(33243586)}");
-            Console.WriteLine($"Kata Human Readable Num: {KataHumanReadableTime.formatDuration2(33243586)}");
-            Console.WriteLine($"Kata Human Readable Num: {KataHumanReadableTime.formatDuration3(33243586)}");
+        //    Console.WriteLine($"Kata Human Readable Num: {KataHumanReadableTime.formatDuration(33243586)}");
+        //    Console.WriteLine($"Kata Human Readable Num: {KataHumanReadableTime.formatDuration2(33243586)}");
+        //    Console.WriteLine($"Kata Human Readable Num: {KataHumanReadableTime.formatDuration3(33243586)}");
 
-            Console.WriteLine($"Kata Int to String Parser: {KataParseIntReloaded.ParseInt("six hundred sixty-six thousand six hundred sixty-six")}");
-            Console.WriteLine($"Kata Int to String Parser: {KataParseIntReloaded.ParseInt("Two hundred Forty-six")}");
-            Console.WriteLine($"Kata Int to String Parser: {KataParseIntReloaded.ParseInt("Zero")}");
+        //    Console.WriteLine($"Kata Int to String Parser: {KataParseIntReloaded.ParseInt("six hundred sixty-six thousand six hundred sixty-six")}");
+        //    Console.WriteLine($"Kata Int to String Parser: {KataParseIntReloaded.ParseInt("Two hundred Forty-six")}");
+        //    Console.WriteLine($"Kata Int to String Parser: {KataParseIntReloaded.ParseInt("Zero")}");
 
-            Console.WriteLine($"Kata Next Big Number (2071): {KataNextBigNumber.NextBiggerNumber(2071)}");
-            Console.WriteLine($"Kata Strip Comments: {KataStripComments.StripComments(
-                "a \n b \nc ", ["#", "$"])}");
-        }
+        //    Console.WriteLine($"Kata Next Big Number (2071): {KataNextBigNumber.NextBiggerNumber(2071)}");
+        //    Console.WriteLine($"Kata Strip Comments: {KataStripComments.StripComments(
+        //        "a \n b \nc ", ["#", "$"])}");
+        //}
 
         public static string PrinterError(String s)
         {
